d4387d1cf7dbe2af788fbe1df2b550afbf531cb7
Benjamin Odell <benjamin.m.odell@proton.me>
2026-02-04 15:26:19 -0600

    Sorted movies into five categories

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ budgets = data['budget']
 #prints scores and budgets
 print(scores)
 print(budgets)
+
+print(data['imdb_score'].describe())
+
+
+#we will split  movies into 5 categories
+#masterpiece >9
+#great >8
+# good >7
+# ok >5
+#bad <= 5
+
+#imdb scores in the csv are from 0-1
+#multiply all scores by ten to get scores from 0-10
+data['imdb_score'] = data['imdb_score'].apply(lambda x: x * 10)
+
+masterpiece_movies = data[data['imdb_score'] >= 9]
+great_movies = data[(data['imdb_score'] >= 8) & (data['imdb_score'] < 9)]
+good_movies = data[(data['imdb_score'] >= 7) & (data['imdb_score'] < 8)]
+ok_movies = data[(data['imdb_score'] >= 5) & (data['imdb_score'] < 7)]
+bad_movies = data[data['imdb_score'] < 5]
